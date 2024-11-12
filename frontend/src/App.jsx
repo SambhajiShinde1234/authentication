1c15085b2d436140fe4e2afb29c9f0d18681c07d
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-11-12 23:57:50 +0530

    setup routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/login';
+import Register from './pages/register';
+
 const App = () => {
   return (
-    <div>authentication - app</div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
