9b3e4f976649e535d34ab3d75b480dd9b65ee174
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-11-14 19:04:01 +0530

    added api calls

---
+++ frontend/src/http/api.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BASE_BACKEND_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem('accessToken');
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
+
+export const POST = async (endpoint, payload) => {
+  try {
+    const response = api.post(endpoint, payload);
+    return response?.data;
+  } catch (error) {
+    const errorMessage =
+      error?.response?.data?.message || 'Something went wrong';
+    return errorMessage;
+  }
+};
+
+export const GET = async (endpoint) => {
+  try {
+    const response = api.get(endpoint);
+    return response?.data;
+  } catch (error) {
+    const errorMessage =
+      error?.response?.data?.message || 'Something went wrong';
+    return errorMessage;
+  }
+};
